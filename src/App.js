bd3e04a3077144ec17b89f984f734426e16694c1
Andres <afcajamarcar@unal.edu.co>
2020-10-10 00:52:44 -0500

    added material ui, and text inputs for the form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import './styles/App.scss';
+import { FormControl, FormGroup, InputLabel, Input, Button } from '@material-ui/core';
 
 function App() {
+  console.log('rendering...');
   return (
     <div className="App">
-      <h3>simple-form</h3>
+      <FormGroup>
+        <FormControl className="justify">
+          <InputLabel htmlFor="student-name" >Nombre completo</InputLabel>
+          <Input type="text" id="student-name"/>
+        </FormControl>
+        <FormControl className="justify">
+          <InputLabel htmlFor="student-code" >Código</InputLabel>
+          <Input type="text" id="student-code"/>
+        </FormControl>
+        <FormControl className="justify">
+          <InputLabel htmlFor="student-help" >Ayuda de interés</InputLabel>
+          <Input type="text" id="student-help"/>
+        </FormControl>
+        <FormControl className="justify">
+          <InputLabel htmlFor="student-assignment" >Asignatura</InputLabel>
+          <Input type="text" id="student-assignment"/>
+        </FormControl>
+        <FormControl className="justify">
+          <InputLabel htmlFor="student-course" >Grupo</InputLabel>
+          <Input type="text" id="student-course"/>
+        </FormControl>
+        {/* TODO implement image loader */}
+        <FormControl className="justify">
+          <h3>imagen del carne</h3>
+        </FormControl>
+        <Button variant="contained" color="primary">Guardar</Button>
+      </FormGroup>
     </div>
   );
 }
