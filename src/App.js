d08c23145f700557f87f212a093a6cf0cd9367ad
Andres <afcajamarcar@unal.edu.co>
2020-10-11 00:14:11 -0500

    implement controlled components by defining a single handleChange method and then passing it to onChange property of every input

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
-import React from 'react';
 import './styles/App.scss';
+import React, { useState } from 'react';
 import { FormControl, FormGroup, InputLabel, Input, Button } from '@material-ui/core';
 
 function App() {
   console.log('rendering...');
+  let [studentInfo, setStudentInfo] = useState({
+    name: '',
+    code: '',
+    help: '',
+    assignment: '',
+    course: '',
+    image: ''
+  });
+
+  const handleChange = e => {
+    setStudentInfo({
+      ...studentInfo,
+      [e.target.name]: e.target.value
+    });
+  }; 
+
   return (
     <div className="App">
       <FormGroup>
         <FormControl className="justify">
           <InputLabel htmlFor="student-name" >Nombre completo</InputLabel>
-          <Input type="text" id="student-name"/>
+          <Input type="text" name="name" id="student-name" onChange={e => handleChange(e)}/>
         </FormControl>
         <FormControl className="justify">
           <InputLabel htmlFor="student-code" >Código</InputLabel>
-          <Input type="text" id="student-code"/>
+          <Input type="text" name="code" id="student-code" onChange={e => handleChange(e)}/>
         </FormControl>
         <FormControl className="justify">
           <InputLabel htmlFor="student-help" >Ayuda de interés</InputLabel>
-          <Input type="text" id="student-help"/>
+          <Input type="text" id="student-help" name="help" onChange={e => handleChange(e)}/>
         </FormControl>
         <FormControl className="justify">
           <InputLabel htmlFor="student-assignment" >Asignatura</InputLabel>
-          <Input type="text" id="student-assignment"/>
+          <Input type="text" id="student-assignment" name="assignment" onChange={e => handleChange(e)}/>
         </FormControl>
         <FormControl className="justify">
           <InputLabel htmlFor="student-course" >Grupo</InputLabel>
-          <Input type="text" id="student-course"/>
+          <Input type="text" id="student-course" name="course" onChange={e => handleChange(e)}/>
         </FormControl>
         {/* TODO implement image loader */}
         <FormControl className="justify">
